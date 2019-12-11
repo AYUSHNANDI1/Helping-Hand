e922c45817a87e61d549dc08db0ce678647472bc
AYUSHNANDI1 <ayushnandikkr@gmail.com>
2019-12-11 14:10:32 +0100

    Continuation of v-1.0

---
+++ makedir.py
@@ -0,0 +1,3 @@
+import os
+
+os.mkdir('/tmp/Practice_Ayush/majordir/GIT/Helping-Hand/haaaaaaa')
--- test.py
+++ test.py
@@ -9,32 +9,35 @@ class MoveFile:
 	def move(self):
 		source = '/tmp/Practice_Ayush/majordir/GIT/Helping-Hand'
 		dest1 = '/tmp/Practice_Ayush/production/scripts'
-		if os.path.isdir(dest1):
-			os.rmdir(dest1)
-
-			files = os.listdir(source)
-                        for f in files:
-                                if os.path.isdir(source+'/'+f):
-                                #print(f)
-                                        shutil.rmtree(dest1, True)
-                                        shutil.copytree(source+'/'+f,dest1,symlinks=False,ignore=None)
-                                else:
-                                        shutil.copy(source+'/'+f,dest1)
-
+		if not os.path.isdir(dest1):
+			os.mkdir(dest1, 0o755)
+			print ("Directory Script made!")
+			#shutil.rmtree(dest1)
 		else:
+			print("Deletiing old directory!")
+			shutil.rmtree(dest1)
+			#est1='/tmp/Practice_Ayush/production/scripts'
+			os.mkdir(dest1, 0o755)
+			print("Created new directory Script!")
 
-			files = os.listdir(source)
-			for f in files:
-				if os.path.isdir(source+'/'+f):
-				#print(f)
-					shutil.rmtree(dest1, True)
-					shutil.copytree(source+'/'+f,dest1,symlinks=False,ignore=None)
-				else:
-					shutil.copy(source+'/'+f,dest1)
+			
+		files = os.listdir(source)
+                for f in files:
+			if os.path.isdir(source+'/'+f):
+				shutil.rmtree(dest1, True)
+                                shutil.copytree(source+'/'+f,dest1,symlinks=False,ignore=None)
+                        else:
+                                shutil.copy(source+'/'+f,dest1)
+		'''with tarfile.open("samp.tar", "w:gz") as tar:
+			tar.add('/tmp/Practice_Ayush/production/', arcname=os.path('/tmp/Practice_Ayush/production/'))'''
 
-		tar = tarfile.open("scripts.tar", "w:tar")
-		for filename in os.listdir(dest1):
-			if filename=="scripts":
+		directory=str('/tmp/Practice_Ayush/production/scripts')
+		tar = tarfile.open("/tmp/Practice_Ayush/production/Scripts.tar", "w:gz")
+		#print(os.listdir(directory))
+		for filename in os.listdir(directory):
+			#print (filename)
+			#if filename=="scripts":
+				#print("Found!")
                 		tar.add(filename)
-
+				exit()
 		tar.close()
