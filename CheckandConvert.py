fdfe04e287fb2ee3eab302ba811dc27b898fb4bb
AYUSHNANDI1 <ayushnandikkr@gmail.com>
2019-12-13 09:01:08 +0100

    13/12/2019

--- CheckandConvert.py
+++ CheckandConvert.py
@@ -2,27 +2,16 @@ from __future__ import print_function
 from test import MoveFile
 import os
 import subprocess
-#branch=raw_input("Enter branch_name:")
-
-#Filename=raw_input("Enter file names:")
-
-
-#get_branch= os.system('git rev-parse --abbrev-ref HEAD')
-#getpwat=os.system('pwd')
-
-#get_new_branch= (str(get_branch).rstrip('\n0'))
-
-#print ("Yes")
 proc = subprocess.Popen(["git rev-parse --abbrev-ref HEAD"], stdout=subprocess.PIPE, shell=True)
 (out, err) = proc.communicate()
 get_branch=out.strip('\n')
 print (get_branch)
 
 if get_branch=="master":
-	print("Here")
+	#print("Here")
 	#print ("Make sure that you have saved changes to master!"),
 	#print ("If sure key in Y/N")
-	status = raw_input("Make sure that you have saved changes to master! If sure, key in Y/N")  # This variable takes in yes or no from user
+	status = raw_input("Make sure that you have saved changes to master! If sure, key in Y/N:")  # This variable takes in yes or no from user
 	if status=="Y":
 		print("We are now ready to move files to Packaging folder!")
 
