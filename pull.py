68f05b193d879e51bd14d480ef39558d6bd25a27
AYUSHNANDI1 <ayushnandikkr@gmail.com>
2019-12-16 08:03:00 +0100

    16/12/19

--- pull.py
+++ pull.py
@@ -4,13 +4,17 @@ import sys
 import os
 
 class pullChanges:
-	def changePull(self):
-		a=str(sys.argv[1])
-		cmd = "git pull {0}".format(a)
-		proc = subprocess.Popen([cmd], stdout=subprocess.PIPE, shell=True)
-		(out, err) = proc.communicate()
-		status=out.strip('\n')
-		print (status)
-
-
-
+        def changePull(self):
+		#parser1=argparse.ArgumentParser()
+		#parser1.add_argument('-p')
+		#pull_parser = parser1.parse_args()
+		#cmd=pull_parser.p
+                #a=str(sys.argv[1])
+                #cmd = "git checkout {0}".format(a)
+		#print(cmd)
+                proc = subprocess.Popen(["git pull"], stdout=subprocess.PIPE, shell=True)
+                (out, err) = proc.communicate()
+                status=out.strip('\n')
+                print (status)
+	#a=str(sys.argv[1])
+	#changePull()
