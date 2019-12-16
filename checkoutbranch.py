755ea3880320adb0d1662462b83e70ab38cc2098
AYUSHNANDI1 <ayushnandikkr@gmail.com>
2019-12-16 11:20:15 +0100

    NewCommit

--- checkoutbranch.py
+++ checkoutbranch.py
@@ -5,6 +5,7 @@ import os
 
 class checkout:
         def chkout(self):
+
                 parser1=argparse.ArgumentParser()
                 parser1.add_argument('-co')
                 pull_parser = parser1.parse_args()
@@ -12,10 +13,18 @@ class checkout:
                 #a=str(sys.argv[1])
                 #cmd = "git checkout {0}".format(a)
                 #rint(cmd)
-                proc = subprocess.Popen(["git checkout"+' '+cmd], stdout=subprocess.PIPE, shell=True)
+                proc = subprocess.Popen(["git add -A"], stdout=subprocess.PIPE, shell=True)
                 (out, err) = proc.communicate()
                 status=out.strip('\n')
-                print (status)
+                proc2 = subprocess.Popen(["git commit -m 'NewCommit'"], stdout=subprocess.PIPE, shell=True)
+                (out2, err2) = proc2.communicate()
+                status2=out.strip('\n')
+                print ("ADDED AND COMMITED")
+                print(status2)
+                proc3 = subprocess.Popen(["git checkout"+' '+cmd], stdout=subprocess.PIPE, shell=True)
+                (out3, err3) = proc3.communicate()
+                status3=out.strip('\n')
+                print (status3)
         #a=str(sys.argv[1])
         #changePull()
 
