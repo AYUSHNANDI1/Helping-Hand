7892ba33d9e44f01aebb68c458115ba92e50d4bd
AYUSHNANDI1 <ayushnandikkr@gmail.com>
2019-12-17 12:32:30 +0100

    16/12/19

--- CheckandConvert.py
+++ CheckandConvert.py
@@ -1,10 +1,7 @@
 from __future__ import print_function
-#from test import MoveFile
-#from pull import pullChanges
-#from checkoutbranch import checkout
 from Common_folder import allfunction
 import os
 import subprocess
 
-c = allfunction()  # objecrt created for checkout class
+c = allfunction()  
 c.convertTar() 
